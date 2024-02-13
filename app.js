ea171847d30654979e2e66a43d0c9ced1b8765b5
Diamond-Dan <dreti.biz@gmail.com>
2024-02-12 22:44:48 -0800

    Switched to handle bars

--- app.js
+++ app.js
@@ -6,6 +6,14 @@
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 8884;                 // Set a port number at the top so it's easy to change in the future
+
+// app.js
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
 // Database
 var db = require('./database/db-connector')
 /*
@@ -13,9 +21,9 @@ var db = require('./database/db-connector')
 */
 app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
     {
-        query1 ='SELECT * FROM Tickets;';
-        db.pool.query(query1,function(err, results, fields){
-            res.send(JSON.stringify(results));      // This function literally sends the string "The server is running!" to the computer
+       let query1 ='SELECT * FROM Tickets;';
+        db.pool.query(query1,function(err, rows, fields){
+            res.render('index', {data: rows});         // This function literally sends the string "The server is running!" to the computer
         });
     });                                         // requesting the web site.
 
