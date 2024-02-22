fd3fe7f0a10d2d9998e8b6665e128f65484a5dce
Khaled Hassan <hassankh@flip3.engr.oregonstate.edu>
2024-02-22 11:36:15 -0800

    added drop downs for tickets and chats pages

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 8864;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 2564;                 // Set a port number at the top so it's easy to change in the future
 
 // app.js
 
@@ -31,19 +31,44 @@ app.get('/', function(req, res)                 // This is the basic syntax for 
             res.render('index', {data: rows});         // This function literally sends the string "The server is running!" to the computer
         });
     });            
+
 app.get('/tickets', function(req, res)                 // This is the basic syntax for what is called a 'route'
-    {
-       let query1 ='SELECT * FROM Tickets;';
-        db.pool.query(query1,function(err, rows, fields){
-            res.render('tickets', {data: rows});         // This function literally sends the string "The server is running!" to the computer
-        });
-    });                                         // requesting the web site.
+{
+    let query1 = "select * from Tickets"
+    let query2 = "select * from Users"
+    let query3 = "select * from Tags"
+    let query4 = "select * from Agents"
+    db.pool.query(query1, function(error, rows, fields){
+        let tickets = rows;
+        db.pool.query(query2, (error, rows, fields) => {
+            let users = rows;
+            db.pool.query(query3, (error, rows, fields) => {
+                let tags = rows
+                db.pool.query(query4, (error, rows, fields) => {
+                    let agent = rows
+                    return res.render("tickets", {data: tickets, user_ids: users, department: tags, agents:agent})
+                })
+            })
+        })
+    })
+}); 
+
 
 
 app.get('/ticket_chats', (req, res) => {
-    let query2 ='SELECT * FROM Ticket_Chats;';
-        db.pool.query(query2,function(err, rows2, fields){
-            res.render('ticket_chats', {data: rows2});         // This function literally sends the string "The server is running!" to the computer
+    let query1 ='SELECT * FROM Ticket_Chats;';
+    let query2 = "select * from Users";
+    let query3 = "select * from Agents";
+        db.pool.query(query1,function(error, rows, fields){
+            let tickets = rows
+            db.pool.query(query2, (error, rows, fields) => {
+            let user = rows
+            db.pool.query(query3, (error, rows, fields) => {
+                let agent = rows
+                console.log(agent)
+                return res.render("ticket_chats", {data: tickets, users: user, agents:agent})
+            })    
+           })
         });
     });
 
