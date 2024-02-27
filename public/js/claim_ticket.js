067bc4849679fa3c9289179075f2cae91a8fb5a8
coneheadlarry <KhHassan@protonmail.com>
2024-02-26 20:41:43 -0800

    fixed

--- public/js/claim_ticket.js
+++ public/js/claim_ticket.js
@@ -1,4 +1,3 @@
-// claim_ticket.js
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('add_agent_ticket_table').addEventListener('submit', function(e) {
         e.preventDefault();
@@ -14,17 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         }
 
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', '/claim-ticket-ajax', true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
+        let xhttp  = new XMLHttpRequest();
+        xhttp .open('POST', '/claim-ticket-ajax', true);
+        xhttp .setRequestHeader('Content-Type', 'application/json');
+        xhttp .onreadystatechange = function () {
+            if (xhttp .readyState === 4 && xhttp .status === 200) {
                 window.alert('Ticket claimed successfully.');
-            } else if (xhr.readyState === 4) {
+            } else if (xhttp .readyState === 4) {
                 window.alert('An error occurred while claiming the ticket.');
             }
         };
         let data = JSON.stringify({ agent_id: agentId, ticket_id: ticketId });
-        xhr.send(data);
+        xhttp .send(data);
     });
 });
